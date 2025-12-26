fc3978537bd97cd89888f22ab4583665599e903a
Guilherme Alves <guilhermealves@MacBook-Air-de-Guilherme.local>
2025-12-25 22:46:57 -0300

    feat: add user schema

---
+++ src/models/users/user.schema.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+import { IUser } from "./user.interface"
+
+const UserSchema = new Schema<IUser>(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model<IUser>("User", UserSchema);
